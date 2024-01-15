f8f0f17698a70a18e58fa571572c2284b4806af1
tjhind <tjhind9@sky.com>
2024-01-15 12:18:37 +0000

    add completed task 3

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,6 +3,7 @@ const request = require("supertest");
 const db = require("../db/connection.js");
 const seed = require("../db/seeds/seed.js");
 const data = require("../db/data/test-data");
+const { readEndpoints } = require("../app/utils/app.utils.js");
 
 beforeEach(() => seed(data));
 afterAll(() => db.end());
@@ -15,6 +16,19 @@ describe("/api/*", () => {
       .then((response) => {
         expect(response.body.msg).toBe("invalid path");
       });
+  });
+});
+
+describe("/api", () => {
+  test("GET 200: responds with a description of all other endpoints available", () => {
+    return readEndpoints().then((result) => {
+      return request(app)
+        .get("/api")
+        .expect(200)
+        .then((response) => {
+          expect(response.body.endpoints).toEqual(result);
+        });
+    });
   });
 });
 
