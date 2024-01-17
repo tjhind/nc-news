fa4815b60291f7ffa4f2b077891b3c01dd011626
tjhind <tjhind9@sky.com>
2024-01-17 08:54:21 +0000

    add completed task 11

--- app/models/articles.models.js
+++ app/models/articles.models.js
@@ -18,7 +18,10 @@ exports.fetchArticles = (topic) => {
 
 exports.fetchArticleById = (article_id) => {
   return db
-    .query(`SELECT * FROM articles WHERE article_id = $1`, [article_id])
+    .query(
+      `SELECT articles.*, COUNT(comments.comment_id) AS comment_count FROM comments RIGHT JOIN articles ON comments.article_id=articles.article_id WHERE articles.article_id = $1 GROUP BY articles.article_id`,
+      [article_id]
+    )
     .then((article) => {
       if (!article.rows.length) {
         return Promise.reject({ status: 404, msg: "Article does not exist" });
