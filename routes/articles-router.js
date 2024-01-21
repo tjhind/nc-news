292c57f25689d22fd8745096e1f3c669438ec6af
tjhind <tjhind9@sky.com>
2024-01-21 13:04:30 +0000

    update router

--- routes/articles-router.js
+++ routes/articles-router.js
@@ -3,6 +3,7 @@ const {
   getArticles,
   editArticleById,
   postNewArticle,
+  deleteArticleById,
 } = require("../app/controllers/articles.controllers");
 const {
   getCommentsByArticleId,
@@ -13,7 +14,11 @@ const articlesRouter = require("express").Router();
 
 articlesRouter.route("/").get(getArticles).post(postNewArticle);
 
-articlesRouter.route("/:article_id").get(getArticleById).patch(editArticleById);
+articlesRouter
+  .route("/:article_id")
+  .get(getArticleById)
+  .patch(editArticleById)
+  .delete(deleteArticleById);
 
 articlesRouter
   .route("/:article_id/comments")
